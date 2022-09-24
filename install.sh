9dd63ede368d0f0060c798b79362a454142eb66d
Ptkatz <112046443+Ptkatz@users.noreply.github.com>
2022-09-24 08:38:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,12 @@ export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
 
 mkdir -p out/master out/server out/puppet 
-cd Orca_Master && go build -o ../out/master/Orca_Master && cd ..
-cd Orca_Server && go build -o ../out/server/Orca_Server && cd ..
-cd Orca_Puppet && go build -o ../out/puppet/Orca_Puppet && cd ..
+cd Orca_Master && go build -o ../out/master/Orca_Master -ldflags "-s -w" && cd ..
+cd Orca_Server && go build -o ../out/server/Orca_Server -ldflags "-s -w" && cd ..
+cd Orca_Puppet && go build -o ../out/puppet/Orca_Puppet_linux_x64 -ldflags "-s -w" && cd ..
+set GOARCH=amd64
+set GOOS=windows
+cd Orca_Puppet && go build -o ../out/puppet/Orca_Puppet_win_x64.exe -ldflags "-s -w" && cd ..
 
 cp -r Orca_Master/3rd_party out/master/3rd_party
 cp -r Orca_Server/db out/server/db
