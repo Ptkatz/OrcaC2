ee52f296645108d1c297e2e1fe18fcf3f9bf73b4
Ptkatz <112046443+Ptkatz@users.noreply.github.com>
2022-09-24 08:59:55 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,12 +3,12 @@ md out\master && md out\server && md out\puppet
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
-cd Orca_Master\ && go build -o ..\out\master\Orca_Master.exe -ldflags "-s -w" && cd ..
-cd Orca_Server\ && go build -o ..\out\server\Orca_Server.exe -ldflags "-s -w" && cd ..
-cd Orca_Puppet\ && go build -o ..\out\puppet\Orca_Puppet.exe -ldflags "-s -w" && cd ..
+cd Orca_Master\ && go build -o ..\out\master\Orca_Master_win_x64.exe -ldflags "-s -w" && cd ..
+cd Orca_Server\ && go build -o ..\out\server\Orca_Server_win_x64.exe -ldflags "-s -w" && cd ..
+cd Orca_Puppet\ && go build -o ..\out\puppet\Orca_Puppet_win_x64.exe -ldflags "-s -w" && cd ..
 set GOARCH=amd64
 set GOOS=linux
-cd Orca_Puppet\ && go build -o ..\out\puppet\Orca_Puppet -ldflags "-s -w" && cd ..
+cd Orca_Puppet\ && go build -o ..\out\puppet\Orca_Puppet_linux_x64 -ldflags "-s -w" && cd ..
 
 xcopy /s /y Orca_Master\3rd_party\ out\master\3rd_party\
 xcopy /s /y Orca_Server\db\ out\server\db\
